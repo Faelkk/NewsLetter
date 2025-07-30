d9133266bf636bc967963a326753b63df12e0cf6
faelkkk <Achten.rafael16@gmail.com>
2025-07-30 15:02:46 -0300

    fix: remove using duplicate namespace

--- src/Newsletter.Presentation/Controllers/UserController.cs
+++ src/Newsletter.Presentation/Controllers/UserController.cs
@@ -67,15 +67,3 @@ public class UsersController : ControllerBase
     }
 }
 
-
-namespace Newsletter.Presentation.DTOS;
-
-public record UpdateSubscriptionRequest(
-    Guid UserId,
-    string? ExternalSubscriptionId,
-    string? Provider,
-    string? Status,
-    DateTime? StartedAt,
-    DateTime? ExpiresAt,
-    DateTime? CanceledAt
-);
