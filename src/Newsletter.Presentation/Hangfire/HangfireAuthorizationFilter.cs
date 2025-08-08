90a9a09e729b6db165f2afe3e71fe18b4087d3dc
Faelkk <Achten.rafael16@gmail.com>
2025-08-08 19:54:14 -0300

    refactor: use authorization to see hangfire dashboard

--- src/Newsletter.Presentation/Hangfire/HangfireAuthorizationFilter.cs
+++ src/Newsletter.Presentation/Hangfire/HangfireAuthorizationFilter.cs
@@ -5,12 +5,10 @@ namespace Newsletter.Presentation.Hangfire;
 public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
 {
     public bool Authorize(DashboardContext context)
-    {
-        // var httpContext = context.GetHttpContext();
-        //
-        // return httpContext.User.Identity?.IsAuthenticated == true &&
-        //        httpContext.User.IsInRole("Admin");
+    { var httpContext = context.GetHttpContext();
+
+ return httpContext.User.Identity?.IsAuthenticated == true &&
+       httpContext.User.IsInRole("Admin");
         
-        return true; 
     }
 }
